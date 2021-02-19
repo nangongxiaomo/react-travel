00d6570d8d0fc7a2223dd7f788bfa43e47a1f706
姜鑫 <1457960500@qq.com>
2021-02-19 19:43:15 +0800

    按需加载路由 loading状态

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,25 @@ import { Footer, Header } from './components'
 import { BrowserRouter, Switch } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
 import routes from './router'
+import { Spin } from 'antd'
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Suspense fallback={<div>loading...</div>}>
+      <Suspense
+        fallback={
+          <Spin
+            style={{
+              position: 'fixed',
+              top: '50%',
+              left: '50%',
+              transform: 'translate3d(-50%, -50%, 0)'
+            }}
+            size="large"
+          />
+        }
+      >
         <Switch>{renderRoutes(routes)}</Switch>
       </Suspense>
       <Footer />
